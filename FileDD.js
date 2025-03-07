dcd333daab25fe7d7de50addbe46b896ef42db39
ohneta <ohneta@iinalab.jp>
2025-03-08 00:03:22 +0900

    1st commit

---
+++ FileDD.js
@@ -0,0 +1,223 @@
+/**
+ * <input>要素の type="file"型拡張
+ * 画像ファイルドラッグ＆ドロップ対応 ファイル選択カスタム要素
+ *
+ * <file-dd>
+ *  要素名 file-dd
+ *  ファイル名 FileDD.js
+ *  属性
+ *    accept 固有ファイル種別指定子。<input type="file">のaccept属性と同等
+ *    name   <input type="file">のname属性と同等
+ *    img-id プレビューimg要素のid属性。このIDで指定されたimg要素にプレビューデータを設定される。
+ *  例 
+ *     <div>
+ *       <img id="preview" />
+ *       <file-dd name="imagefile" accept="image/png, image/jpeg" img-id="preview" />
+ *     </div>
+ *
+ *  ::part(file-button)
+ *    file-dd要素のshadowDOM内の<input type="file">へは "file-button" として::part()で参照可能。
+ */
+
+class FileDD extends HTMLElement {
+
+  static formAssociated = true; // formに参加表明
+  static observedAttributes = [ "accept", "name",  "img-id" ];
+
+  accept = null;
+  fileInputElement = null;
+  imgId = null;
+  imgElement = null;
+
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+// console.log("--");
+// console.log("connectedCallback()");
+    if (this.hasAttribute('name')) {
+      if (this.fileInputElement != null) {
+        this.fileInputElement.name = this.getAttribute('name');
+      }
+    }
+
+    this._updateRendering();
+  }
+
+  disconnectedCallback() {
+  }
+
+  adoptedCallback() {
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+// console.log("--");
+// console.log("attributeChangedCallback()");
+// console.log(`name = ${name}`);
+// console.log(`oldValue = ${oldValue}`);
+// console.log(`newValue = ${newValue}`);
+    if (name == 'accept') {
+      this.accept = newValue;
+    }
+    if (name == 'img-id') {
+      this.imgId = newValue;
+    }
+    if (name === 'name') {
+      if (this.fileInputElement != null)
+        this.fileInputElement.name = newValue;
+    }
+
+//    this._updateRendering();
+  }
+
+
+  get accept() {
+    return this.accept;
+  }
+  set accept(v) {
+    this.setAttribute("accept", v);
+  }
+
+  // `name` 属性を取得/設定
+  get name() {
+    return this.getAttribute('name');
+  }
+
+  set name(val) {
+    this.setAttribute('name', val);
+  }
+
+  formAssociatedCallback() {
+    this.internals = this.attachInternals();
+    if (this.fileInputElement != null)
+      this.internals.setFormValue(this.fileInputElement.files.item(0));  // とりあえず1ファイルのみ対応
+  }
+
+
+  //--------------------
+
+  _updateRendering() {
+    // LightDOMにあるプレビュー用のimgタグをidで取得しておく
+    let documentImgElement = null;
+    if (this.imgId != null) {
+      documentImgElement = document.getElementById(this.imgId);
+      if (documentImgElement != null) {
+        this.imgElement = documentImgElement;
+      }
+    }
+
+    if (this.shadowRoot == null)
+      this.attachShadow({mode: 'open'});
+
+    const parentElement = document.createElement("div");
+    this.fileInputElement = document.createElement('input');
+    this.fileInputElement.setAttribute('part', 'file-button');
+
+
+    // drag&dropイベント
+    parentElement.addEventListener('dragover', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      parentElement.style.border = "solid";
+	  });
+    parentElement.addEventListener('dragenter', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      parentElement.style.border = "solid";
+	  });
+    parentElement.addEventListener('dragleave', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      parentElement.style.border = "";
+	  });
+    parentElement.addEventListener('drop', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      parentElement.style.border = "";
+
+      // ドロップオブジェクトのうち、ファイルのみを取得
+      let dropFiles = [];
+      if (event.dataTransfer.items) {
+        [...event.dataTransfer.items].forEach((item, i) => {
+          if (item.kind === "file") {
+            const file = item.getAsFile();
+            dropFiles[i] = file;
+          }
+        });
+      } else {
+        [...evt.dataTransfer.files].forEach((file, i) => {
+          dropFiles[i] = file;
+        });
+      }
+
+      // imageファイルか？
+      let isImageFileFlag = false;
+      if (this.accept == 'image/*') {   // "image/*" 対応
+        const types = dropFiles[0].type.split('/');
+        if (types[0] == 'image') {
+          isImageFileFlag = true;
+        }
+      } else {                          // "image/png, image/jpeg ..." など accept の個別対応
+        if (this.accept.includes(dropFiles[0].type)) {
+          isImageFileFlag = true;
+        }
+      }
+
+      if (isImageFileFlag) {
+        // input filesの置き換え
+        const dataTransfer = new DataTransfer();
+        Array.from(dropFiles).forEach(file => {
+          dataTransfer.items.add(file);
+        });
+        this.fileInputElement.files = dataTransfer.files;
+        this.internals.setFormValue(this.fileInputElement.files.item(0));
+        if (this.imgElement != null) {
+          this._previewImage(this.imgElement, dropFiles[0]);
+        }
+      } else {
+        console.log("dropped files not accepted");
+      }
+
+    });
+
+    {
+      this.fileInputElement.type = 'file';
+      this.fileInputElement.accept = this.accept;
+      this.fileInputElement.addEventListener('change', (event) => {
+        this.internals.setFormValue(this.fileInputElement.files.item(0));
+        this._previewImage(this.imgElement, event.target.files[0]);
+      });
+    }
+
+    const fileInputDivElement = document.createElement('div');
+    {
+      this.fileInputElement.textAlign = "left";
+      fileInputDivElement.appendChild(this.fileInputElement);
+      fileInputDivElement.justifyContent = "start";
+    }
+    parentElement.append(fileInputDivElement);
+
+    this.shadowRoot.replaceChildren(parentElement);
+
+  }
+
+  //--------------------
+  /**
+   * プレビュー要素に画像を反映
+   *
+   * @param {Element} imgElement  画像ファイルプレビュー要素
+   * @param {File} file プレビューする画像ファイル
+   * @return {void}
+   */
+  _previewImage(imgElement, file) {
+    imgElement.src = window.URL.createObjectURL(file);
+    imgElement.onload = () => {
+      URL.revokeObjectURL(imgElement.src);
+    };
+  }
+
+}
+
+customElements.define("file-dd", FileDD);
---
+++ README.md
@@ -0,0 +1,64 @@
+# Custom Element for Image File Selection　(画像ファイル選択カスタム要素)
+An extension of `<input type="file">`  
+`<input type="file">`の拡張
+
+## Features (提供機能)
+* File drag-and-drop (ファイルのドロップ)
+* Image file preview (画像ファイルのプレビュー)
+
+### File Drag-and-Drop (ファイルのドロップ)
+You can drop image files within the content area of the element.
+要素のコンテンツ領域で画像ファイルのドロップできます。
+
+### Image File Preview (画像ファイルのプレビュー)
+By specifying the `id` of an `img` element for preview, the selected image file can be displayed.  
+プレビュー用のimg要素のidを指定することで、選ばれた画像ファイルをプレビューできます。
+
+## Script File (スクリプトファイル)
+|||
+|-|-|
+|File Name (ファイル名)|FileDD.js|
+|Element Name (要素名)|file-dd|
+
+## 書式 (Syntax)
+```html
+\<file-dd>\</file-dd>
+```
+
+### Attributes　(属性)
+|||
+|-|-|
+|accept | Specifies allowed file types. Equivalent to the `accept` attribute of `<input type="file">` <br>固有ファイル種別指定子。`<input type="file">`のaccept属性と同等|
+|name   | Equivalent to the `name` attribute of `<input type="file">`　<br>`<input type="file">`のname属性と同等|
+|img-id | The `id` of the preview `img` element. The preview data will be set to the `img` element specified by this id. <br>プレビューimg要素のid属性。このidで指定されたimg要素にプレビューデータを設定される。|
+
+
+## Shadow DOM (::part)
+|||
+|-|-|
+|file-button| The `<input type="file">` inside the shadow DOM of `file-dd` can be referenced using `::part("file-button")`. <br>file-dd要素のshadowDOM内の<input type="file">へは "file-button" として::part()で参照可能。|
+
+
+## Examples (例)
+### Minimal Usage (最小)
+```html
+<div>
+  <file-dd name="imagefile" accept="image/png, image/jpeg"></file-dd>
+</div>
+```
+
+### With Image Preview (画像プレビュー付き)
+```html
+<div>
+  <img id="preview1">
+  <file-dd name="imagefile1" accept="image/*" img-id="preview1"></file-dd>
+
+  <img id="preview2">
+  <file-dd name="imagefile2" accept="image/png, image/jpeg" img-id="preview2"></file-dd>
+ </div>
+```
+
+## more...
+Refer to the included index.html.
+(同梱の [index.html](./index.html) を参照)
+
